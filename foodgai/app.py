151c2c55cf18508e73408055036f01298a58d4e1
VasanthPranavan <47364231+VasanthPranavan@users.noreply.github.com>
2024-05-01 04:26:54 -0500

    Vision Api changes

--- foodgai/app.py
+++ foodgai/app.py
@@ -1,5 +1,12 @@
 from flask import Flask, request, render_template
+from werkzeug.utils import secure_filename
+from pathlib import Path
+import hashlib
+
 import google.generativeai as genai
+import os
+
+
 
 app = Flask(__name__)
 
@@ -104,6 +111,58 @@ def captureCook():
 
         return render_template("captureCook.html", formatted_sections=formatted_sections)
     return render_template("captureCook.html")
+
+
+generation_config_Vision = {
+    "temperature": 1,
+    "top_p": 0.95,
+    "top_k": 0,
+    "max_output_tokens": 8192,
+}
+
+safety_settings_Vision = [
+    {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+    {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+    {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+    {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+]
+
+modelVision = genai.GenerativeModel(
+    model_name="gemini-1.5-pro-latest",
+    generation_config=generation_config_Vision,
+    safety_settings=safety_settings_Vision,
+)
+
+@app.route('/index2', methods=['GET', 'POST'])
+def index2():
+    if request.method == 'POST':
+        
+
+
+
+
+        image_path = Path("image.jpeg")
+        image_part = {
+            "mime_type": "image/jpeg",
+            "data": image_path. read_bytes()
+        }
+
+        prompt_parts = [
+            "You are AI chef and diet expert. Your name is Chef. FoodGAI. The patient has uploaded the an image of the ingregients they have. You need to create a recipe based on the avaialble incredients in the imae.and asked for diet plan. Based on the below details respond starting to welcome them and introduce yourself,  Provide the recipe and instrutions.\n\n Name: Sruthi \nDietary Preferences: vegetarian:\n",
+            image_part
+        ]    
+
+       # print(prompt_parts.text)
+
+        # Generate content
+        result = modelVision.generate_content(prompt_parts)   
+        print(result.text)
+        generated_text = result.text
+            
+        return render_template('index2.html', generated_text=generated_text)
+    
+    return render_template('index2.html')
+
 
 
 if __name__ == "__main__":
